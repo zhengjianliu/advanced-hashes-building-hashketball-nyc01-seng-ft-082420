90a6c709dcf11dc0b66db1f1b8a0bcbb5eea0286
ZHENGJIAN LIU <comezheng@gmail.com>
2020-08-13 22:04:29 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,7 +3,7 @@ require 'pry'
 def game_hash{
   home:{
     team_name: "Brooklyn Nets",
-    colors:["Black", "White"],
+    colors: ["Black", "White"],
     players:[
         {
           player_name: "Alan Anderson", 
